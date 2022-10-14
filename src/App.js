ce192627876f143bfe618d28822eb27ec62d296b
Jevin Jivani <jivani.jevin25@gmail.com>
2022-10-14 19:28:50 +0530

    Basic Setup DONE!!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createTheme, NextUIProvider, Text } from "@nextui-org/react";
+
+const theme = createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      primary: "#0070f3",
+    },
+    space: {},
+    fonts: {},
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NextUIProvider theme={theme}>
+      <div className="App">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </NextUIProvider>
   );
 }
 
